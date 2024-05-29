95c8d639f3d9b0f25c4d4b1b33d777416d374082
Tom Runting <tr1123@lx04.hep.ph.ic.ac.uk>
2024-05-29 12:07:28 +0100

    Add initial fit

--- tasks/combine.py
+++ tasks/combine.py
@@ -3,14 +3,15 @@ import re
 import law
 import luigi
 import shutil
-import subprocess
 from typing import Dict
-from getPOIs import GetWsp, GetT2WOpts
+from getPOIs import GetWsp, GetT2WOpts, GetMinimizerOpts, GetPOIsList, SetSMVals, GetPOIRanges, GetFreezeList
 from tasks.base import BaseTask, ForceableWithNewer
+from tasks.remote import HTCondorCPU
 
 from law.logger import get_logger
 logger = get_logger('luigi-interface')
 
+# TODO tidy this
 # Combine tasks:
 # Ensure cmssetup has been run
 # Ensure combine is installed
@@ -19,23 +20,18 @@ logger = get_logger('luigi-interface')
 
 # The derived classes then actually run the command
 
-
-def source_and_get_environment(script, shell="/bin/bash"):
-    command = [shell, "-c", f"{script} && env"]
-    proc = subprocess.Popen(command, stdout=subprocess.PIPE)
-    for line in proc.stdout:
-        line = line.decode()
-        (key, _, value) = line.partition("=")
-        os.environ[key] = value.strip()
-    proc.communicate()
-
 class CombineBase(BaseTask):
     # Define common parameters
+    MH = luigi.FloatParameter(
+        default="125.38",
+        description="nominal Higgs mass; default is 125.38 (GeV)"
+    )
+    
     channel = luigi.Parameter(
         default="hgg_statonly",
         description="channel to use for the combination; default is hgg_statonly"
     )
-    
+
     inputs = luigi.Parameter(
         default="comb_2021_hgg.inputs.root",
         description="the name of the root inputs file; default is comb_2021_hgg.inputs.root"
@@ -44,6 +40,11 @@ class CombineBase(BaseTask):
     model = luigi.Parameter(
         default="STXStoSMEFTExpandedLinearStatOnly",
         description="model to use for the combination; default is STXStoSMEFTExpandedLinearStatOnly"
+    )
+    
+    types = luigi.Parameter(
+        default="observed",
+        description="type of scan to run; default is observed" # TODO change this to something sensible
     )
     
     def __init__(self, *args, **kwargs):
@@ -57,20 +58,14 @@ class CombineBase(BaseTask):
         # Check if CMSSW_BASE is set, and source it if not
         self.cmssw_base = os.getenv("CMSSW_BASE")
         if self.cmssw_base is None:
-            logger.info("CMSSW_BASE is not set. Attempting to use $CMSSW_PATH from setup script.")
-            cmd = f"cd {os.environ['CMSSW_PATH']};"
-            cmd += "cmsenv;"
-            cmd += f"cd {os.environ['ANALYSIS_PATH']}"
-            source_and_get_environment(cmd)
-            logger.info(f"CMSSW_BASE is now set to {os.getenv('CMSSW_BASE')}")
-        self.cmssw_base = os.getenv("CMSSW_BASE")
-        if self.cmssw_base is None:
-            raise ValueError("CMSSW_BASE could not be set.")
+            raise ValueError("CMSSW_BASE is not set, please run 'cmsenv'")
 
         # Check if combine is installed
         self.combine_path = shutil.which("combine")
         if self.combine_path is None:
             raise ValueError(f"Combine is not installed in {self.cmssw_base}.")
+        else:
+            logger.debug(f"Found combine at {self.combine_path}")
         
         # Define frequently used commands
         self.base_command = f"cd {os.environ['CMSSW_PATH']}; cmsenv; cd {os.environ['ANALYSIS_PATH']}; ulimit -s unlimited; "
@@ -123,18 +118,65 @@ class TextToWorkspace(CombineBase, ForceableWithNewer):
         card_path = card_target.path
         logger.info(f"Running: TextToWorkspace, will output {self.output()}")
 
-        # Don't run if workspace already exists
         wsp_target = self.output()['workspace']
         assert isinstance(wsp_target, law.LocalFileTarget) 
-        if os.path.exists(wsp_target.path) and not self.force:
-            logger.info(f"Workspace already exists for channel [{self.channel}], model [{self.model}]")
-        else:
-            # Build and run the command
-            cmd = self.base_command
-            cmd += (
-                f"{self.time_command} text2workspace.py -o {wsp_target.basename} {card_path} {self.wsp_opts}"
-                f" &> {outdir}/t2w_{self.channel}_{self.model}_local.log;"
-                f" mv {wsp_target.basename} {wsp_target.path}"
-            )
-            self.run_cmd(cmd)
-            
+        # Build and run the command
+        cmd = self.base_command
+        cmd += (
+            f"{self.time_command} text2workspace.py -o {wsp_target.basename} {card_path} {self.wsp_opts}"
+            f" &> {outdir}/t2w_{self.channel}_{self.model}_local.log;"
+            f" mv {wsp_target.basename} {wsp_target.path}"
+        )
+        self.run_cmd(cmd)
+
+class InitialFit(CombineBase, ForceableWithNewer, HTCondorCPU, law.LocalWorkflow):    
+    def create_branch_map(self):
+        return [None] # Single branch, no special data
+    
+    def workflow_requires(self):
+        return []
+    
+    def requires(self):
+        return TextToWorkspace.req(self) # No dependencies for the branch, just the workflow itself
+    
+    def output(self):
+        return {'tree': self.local_target(f"higgsCombine.initial.{self.channel}.{self.model}.{self.types}.MultiDimFit.mH.{self.MH}.123456.root"),
+                'log': self.local_target(f"initialfit_{self.channel}_{self.model}_local.log;")}
+    
+    def run(self):        
+        logger.info(f"Running: InitialFit, will output {self.output()}")
+        channel = self.channel
+        model = self.model 
+        types = self.types
+
+        # Get options from getPOIs 
+        # TODO offload this to base
+        COMBINE_OPTIONS = GetMinimizerOpts(model)
+        COMBINE_POIS = GetPOIsList(model)
+        COMBINE_SET = SetSMVals(model)
+        COMBINE_RANGES = GetPOIRanges(model)
+        COMBINE_FREEZE = GetFreezeList(model)
+        if COMBINE_FREEZE != '':
+            COMBINE_FREEZE = ',' + COMBINE_FREEZE
+        
+        GENERATE_STR = "n;t;toysFrequentist;"
+        if 'observed' in types: GENERATE_STR += ";observed,!,!"
+        if 'prefit_asimov' in types: GENERATE_STR += ";prefit_asimov,-1,!"
+        if 'postfit_asimov' in types: GENERATE_STR += ";postfit_asimov,-1, "
+        input = self.input()
+        assert isinstance(input, law.LocalFileTarget)
+        ws_path = input.path
+
+        # Build and run the command
+        cmd = self.base_command
+        cmd += (
+            f"{self.time_command} combineTool.py -M MultiDimFit "
+            f"-m {self.MH} -d {ws_path} "
+            f"--redefineSignalPOIs {COMBINE_POIS} --setParameters {COMBINE_SET} "
+            f"--setParameterRanges {COMBINE_RANGES} --freezeParameters MH{COMBINE_FREEZE} "
+            f"--generate '{GENERATE_STR}' --saveToys --saveWorkspace {COMBINE_OPTIONS} "
+            f"-n .initial.{channel}.{model} "
+            f"&> {self.output()['tree'].dirname}/initialfit_{channel}_{model}_local.log;"
+        )
+        cmd += f"mv {self.output()['tree'].basename} {self.output()['tree'].path};"
+        self.run_cmd(cmd)
